cd3689caf6c1ada6e998cce485476a37f59f7de5
Jude Ben <jude.ben@shareqube.com>
2016-04-30 17:57:01 +0100

    Leadershift event page

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+/*
+ * Change Navbar color while scrolling
+*/
+
+$(document).ready(
+
+    function(){
+
+        
+        var date  = new Date(Date.UTC(2016, 5 + 1, 13 + 1 ));
+var now   = new Date();
+var diff  = date.getTime()/1000 - now.getTime()/1000;
+
+        console.log("daye" + diff) ;
+
+        var clock = $('.clock').FlipClock(new Date(2016,4,13).getTime()/1000 - new Date().getTime()/1000, {
+        clockFace: 'DailyCounter',
+        countdown: true
+    });
+
+        
+      
+
+
+
+
+
+google.maps.event.addDomListener(window, "load", initialize);
+
+
+    }
+
+
+    );
+
+
+
+
+$(window).scroll(function(){
+	handleTopNavAnimation();
+});
+
+$(window).load(function(){
+	handleTopNavAnimation();
+});
+
+function handleTopNavAnimation() {
+	var top=$(window).scrollTop();
+
+	if(top>10){
+		$('#site-nav').addClass('navbar-solid'); 
+	}
+	else{
+		$('#site-nav').removeClass('navbar-solid'); 
+	}
+}
+
+/*
+ * Registration Form
+*/
+
+$('#registration-form').submit(function(e){
+    e.preventDefault();
+    
+    var postForm = { //Fetch form data
+            'fname'     : $('#registration-form #fname').val(),
+            'lname'     : $('#registration-form #lname').val(),
+            'email'     : $('#registration-form #email').val(),
+            'cell'      : $('#registration-form #cell').val(),
+            'address'   : $('#registration-form #address').val(),
+            'zip'       : $('#registration-form #zip').val(),
+            'city'      : $('#registration-form #city').val(),
+            'program'   : $('#registration-form #program').val()
+    };
+
+    $.ajax({
+            type      : 'POST',
+            url       : './assets/php/contact.php',
+            data      : postForm,
+            dataType  : 'json',
+            success   : function(data) {
+                            if (data.success) {
+                                $('#registration-msg .alert').html("Registration Successful");
+                                $('#registration-msg .alert').removeClass("alert-danger");
+                                $('#registration-msg .alert').addClass("alert-success");
+                                $('#registration-msg').show();
+                            }
+                            else
+                            {
+                                $('#registration-msg .alert').html("Registration Failed");
+                                $('#registration-msg .alert').removeClass("alert-success");
+                                $('#registration-msg .alert').addClass("alert-danger");
+                                $('#registration-msg').show();
+                            }
+                        }
+        });
+});
+
+/*
+ * SmoothScroll
+*/
+
+smoothScroll.init();
+
+
+
+// ==========  START GOOGLE MAP ========== //
+function initialize() {
+    var myLatLng = new google.maps.LatLng(5.054590, 7.926559);
+
+    var mapOptions = {
+        zoom: 14,
+        center: myLatLng,
+        disableDefaultUI: true,
+        scrollwheel: false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+        }
+    };
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: '../assets/images/location-icon.png',
+        title: '',
+    });
+
+}
+
+// ========== END GOOGLE MAP ========== //
